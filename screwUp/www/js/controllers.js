48d918c01486c3ba50a026e2c51d8a7f7fea4b5c
johnde02 <devon@MacBook-Pro.local>
2016-10-29 18:30:51 -0400

    git commit - m"add working map"

--- screwUp/www/js/controllers.js
+++ screwUp/www/js/controllers.js
@@ -90,6 +90,35 @@ var options = {timeout: 10000, enableHighAccuracy: true};
     };
  
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+//wait until the map is loaded
+    google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    animation: google.maps.Animation.DROP,
+                    position: latLng
+                });
+
+            });
+
+            google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    animation: google.maps.Animation.DROP,
+                    position: latLng
+                });
+
+                var infoWindow = new google.maps.InfoWindow({
+                    content: "Here I am!"
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infoWindow.open($scope.map, marker);
+                });
+
+            });
  
   }, function(error){
     console.log("Could not get location");
